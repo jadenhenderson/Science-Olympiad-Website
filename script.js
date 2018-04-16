c95a3a4155930903d8596da2d435ce84069c1909
jadenhenderson <38412915+jadenhenderson@users.noreply.github.com>
2018-04-15 22:38:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4000); // Change image every 5 seconds
+}
+
+}
